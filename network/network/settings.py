a53bf7b20aa00feb6918d0ee19b133bc982d17e5
swoosh1k <lilsmosh228@gmail.com>
2024-02-09 00:17:22 +0300

    Добавил отправку сообщений на gmail, пофиксил registerview, поменял оформление некоторое

--- network/network/settings.py
+++ network/network/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social',
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# core/settings.py
+
+
 
 
 AUTH_USER_MODEL = 'social.User'
@@ -135,6 +140,14 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, "static"),
 ]
-# MEDIA_URL = '/media/'
-#
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'artembandit0@gmail.com'
+EMAIL_HOST_USER = 'artembandit0@gmail.com'
+EMAIL_HOST_PASSWORD = 'foprbwqihmovhirq'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+PASSWORD_RESET_TIMEOUT = 14400
