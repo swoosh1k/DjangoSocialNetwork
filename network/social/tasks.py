6a040f9b07ff4971b1cbc1816209cdb2294aa965
swoosh1k <lilsmosh228@gmail.com>
2024-03-01 00:06:57 +0300

    Celery + отправка новостей(свежых постов)

---
+++ network/social/tasks.py
@@ -0,0 +1,34 @@
+from celery import shared_task
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import send_mail
+from datetime import datetime
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+
+from .models import News, Follower, Post
+
+'''Task, that shows you last friends posts'''
+@shared_task
+def send_email_task():
+    users = News.objects.first().users.all()
+    for user in users:
+        follower = Follower.objects.get(user_id = user.id)
+        followings = follower.followings.all()
+        all_posts = []
+        for man in followings:
+            post = Post.objects.filter(creater_id = man.id).order_by('data_created')
+            if post.exists():
+                post = post.last()
+                all_posts.append(post)
+        all_posts = all_posts[:3]
+        print(all_posts)
+        subject = 'Новая новость!'
+        message = render_to_string('social/send_news_spam.html', {
+            'posts': all_posts,
+            'user': user
+        })
+        email = EmailMessage(subject, message, to=[user.email])
+        if email.send():
+            print(f"Email sent at {datetime.now()} to {user.email}")
+
+
