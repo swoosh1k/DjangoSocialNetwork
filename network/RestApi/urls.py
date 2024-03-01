227943fff592901ed64283d708f323e20d52086a
tema <supron95@gmail.com>
2024-03-01 21:40:07 +0300

    add gitignore

--- network/RestApi/urls.py
+++ network/RestApi/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import PostView, UserView
+from .views import getView, UserView, PostUpdateView
 urlpatterns = [
-    path('', PostView.as_view(), name = 'post-list'),
-    path('userApi/', UserView.as_view(), name = 'user-list')
+    path('', getView.as_view(), name = 'post-list'),
+    path('userApi/', UserView.as_view(), name = 'user-list'),
+    path('posts/<int:pk>/', PostUpdateView.as_view(), name='post-update'),
 
 ]
